4fd70312b259ce53076290faa1dbe733dcc0fa79
athinags <150917990+athinags@users.noreply.github.com>
2025-04-27 15:58:40 -0300

    Delete GameObserver.java

--- GameObserver.java
+++
@@ -1,8 +0,0 @@
-package br.com.trucomineiro.view;
-
-import br.com.trucomineiro.model.GameState;
-import br.com.trucomineiro.model.Table;
-
-public interface GameObserver {
-    void update(GameState state, Table table);
-}
